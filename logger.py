89f49f40a9d9da4b7551a29eac7a9b5303f5a98a
Sam Povilus <povilus@povil.us>
2018-03-29 13:43:55 +0000

    reducing brightness

--- logger.py
+++ logger.py
@@ -39,6 +39,7 @@ pressure = sense.get_pressure()
 temp = sense.get_temperature()
 humidity = sense.get_humidity()
 	
+brightness =50
 while True:
 	pressure = sense.get_pressure()
 	temp = 9.0/5.0 * sense.get_temperature() + 32.0
@@ -48,6 +49,6 @@ while True:
 	if(loopcount %(6) == 0):
 		add_pressure(pressure,temp)
 		add_humidity(humidity,temp)
-	sense.show_message("P:" + "{:0.2f}".format(pressure),text_colour=(255,255,0),back_colour=(0,0,255))
-	sense.show_message("H:" + "{:0.2f}".format(humidity),text_colour=(255,0,0),back_colour=(0,255,255))
-	sense.show_message("T:" + "{:0.2f}".format(temp),text_colour=(0,255,0),back_colour=(255,0,255))
+	sense.show_message("P:" + "{:0.2f}".format(pressure),text_colour=(0,brightness,brightness),back_colour=(0,0,0))
+	sense.show_message("H:" + "{:0.2f}".format(humidity),text_colour=(brightness,0,0),back_colour=(0,0,0))
+	sense.show_message("T:" + "{:0.2f}".format(temp),text_colour=(0,brightness,0),back_colour=(0,0,0))
